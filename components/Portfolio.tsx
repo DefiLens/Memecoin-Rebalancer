f42a957dac5980770ad531b35c662cdb69897ce1
sunnyrk <radadiyasunny970@gmail.com>
2024-10-14 23:28:07 +0530

    add portfolio details

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -6,6 +6,7 @@ import { useAccount, useBalance } from 'wagmi';
 import { FiX } from 'react-icons/fi';
 import Image from 'next/image';
 import { toast } from 'react-toastify';
+import { ICoinDetails } from './rebalance/types';
 
 interface PortfolioProps {
     isOpen: boolean;
@@ -20,37 +21,6 @@ interface TokenBalance {
     image: string;
     price: number;
     value: number;
-}
-
-interface ICoinDetails {
-    id: string;
-    symbol: string;
-    name: string;
-    image: string;
-    current_price: number;
-    market_cap: number;
-    market_cap_rank: number;
-    fully_diluted_valuation: number;
-    total_volume: number;
-    high_24h: number;
-    low_24h: number;
-    price_change_24h: number;
-    price_change_percentage_24h: number;
-    market_cap_change_24h: number;
-    market_cap_change_percentage_24h: number;
-    circulating_supply: number;
-    total_supply: number;
-    max_supply: number;
-    ath: number;
-    ath_change_percentage: number;
-    ath_date: string;
-    atl: number;
-    atl_change_percentage: number;
-    atl_date: string;
-    roi: null;
-    last_updated: string;
-    price_change_percentage_24h_in_currency: number;
-    price_change_percentage_7d_in_currency: number;
 }
 
 const BATCH_SIZE = 20;
