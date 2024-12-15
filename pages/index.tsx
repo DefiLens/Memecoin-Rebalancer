edda7dc1876845d22a46d893c8133d9a3b861a46
sunnyrk <radadiyasunny970@gmail.com>
2024-12-15 20:07:13 +0530

    fix build issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import MainLayout from "../components/layouts/MainLayout";
 import Market from "../components/rebalance/Market";
-import MetaTags from "../components/base/metaTags";
+import MetaTags from "../components/base/MetaTags";
 
 export default function Home() {
   return (
